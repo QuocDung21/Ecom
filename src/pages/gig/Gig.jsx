4d05749f08a211a72013d85bf8556af0611b12ac
QuocDung21 <rongvang2357hn@gmail.com>
2023-02-24 22:20:15 +0700

    remove

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -3,15 +3,7 @@ import React from 'react'
 const Gig = () => {
    return (
       <div className="gig">
-         Gig thay doi nua ne moi moi
-         moiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
          <div>Backend</div>
-         <div>Backend2</div>
-         <div>Backend3</div>
-         <div>Backend4</div>
-         <div>Backend5</div>
-         <div>Backend6</div>
-         <div>Backend7</div>
       </div>
    )
 }
